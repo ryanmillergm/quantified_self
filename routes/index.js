084f76fae6da2903d52d0a8477842e6621d45ad8
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-21 13:18:16 -0600

    Paste in heroku postgres snippets
    Co-authored-by: Ryan Miller <42855291+ryanmillergm@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });

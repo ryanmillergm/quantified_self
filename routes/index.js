7daf08d94f9dd8681f1d55208bffd242ec6b718c
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-21 13:18:38 -0600

    Add /db route from heroku setup guide

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET database */
+router.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 module.exports = router;

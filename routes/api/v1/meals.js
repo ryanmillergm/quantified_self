a456b29c593f8d49e332f621cbec0926beaf6ca6
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-23 17:10:23 -0600

    Fix mealsRouter export & attributes

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -2,22 +2,24 @@
 var express = require("express");
 var router = express.Router();
 var Meal = require("../../../models").Meal;
+var Food = require("../../../models").Food;
 
 /*GET all meals*/
 router.get("/", function(req, res, next) {
   return Meal.findAll({
-    attributes: ["id", "name", "calories"],
-    include: [
-      {
-        model: Foods
-      }
-    ]
+    attributes: ["id", "name"],
+    include: [{
+        model: Food,
+        as: 'foods'
+    }]
   })
-    .then(meals => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).send(JSON.stringify(meals));
-    })
-    .catch(err => {
-      res.status(500).send(JSON.stringify({ error: err }));
-    });
+  .then(meals => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify(meals));
+  })
+  .catch(err => {
+    res.status(500).send(JSON.stringify({ error: err }));
+  });
 });
+
+module.exports = router;
